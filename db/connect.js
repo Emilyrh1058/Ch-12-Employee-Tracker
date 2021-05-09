2a0f43e6fc53c10f24c9fe31513ad343596b0e8c
Emily Salinas <emilyrh1058@gmail.com>
2021-05-09 00:45:44 -0500

    tested in insomnia, emp routes need work

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql2');
 
-require('dotenv').config();
+
+
+require('dotenv').config({ path: '/.env'});
 const password = process.env.PASSWORD;
 
 // CONNECT TO DATABASE
@@ -8,11 +10,17 @@ const db = mysql.createConnection(
 {
   host: 'localhost',
   user: 'root',
-  password: password,
+  password: 'Salemcaiman1619!',
   database: 'emp_db'
 });
 
 
+// const db = require('db')
+// db.connect({
+//   host: process.env.DB_HOST,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+// });
 
 
 module.exports = db;
