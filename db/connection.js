20290d0d333982a324873af8d34bb6c26943bacc
Emily Salinas <emilyrh1058@gmail.com>
2021-07-10 18:39:32 -0500

    adjusted connection.js according to LA

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,5 @@
 const mysql = require('mysql2');
 
-
-
 require('dotenv').config({ path: '/.env'});
 const password = process.env.PASSWORD;
 
@@ -9,18 +7,16 @@ const password = process.env.PASSWORD;
 const db = mysql.createConnection(
 {
   host: 'localhost',
+  port: 3306,
   user: 'root',
   password: 'Salemcaiman1619!',
   database: 'emp_db'
 });
 
-
-// const db = require('db')
-// db.connect({
-//   host: process.env.DB_HOST,
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-// });
+db.connect(function(err) {
+  if (err)
+  throw err
+});
 
 
 module.exports = db;
